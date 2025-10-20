0c6b1196125a7563cf6bf83954afc1f466a565cf
EmaBrites <ebritesotero@alumnos.exa.unicen.edu.ar>
2025-10-20 17:12:49 -0300

    ej2 d y e

--- tp3/src/main/java/com/example/tp3/dto/EstudianteCarreraDTO.java
+++ tp3/src/main/java/com/example/tp3/dto/EstudianteCarreraDTO.java
@@ -3,15 +3,12 @@ package com.example.tp3.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.ToString;
 
 import java.io.Serializable;
+
 @Data
 @AllArgsConstructor
 @Builder
-
-
-
 public class EstudianteCarreraDTO implements Serializable {
     private final int id;
     private final int inscripcion;
--- tp3/src/main/java/com/example/tp3/service/EstudianteService.java
+++ tp3/src/main/java/com/example/tp3/service/EstudianteService.java
@@ -19,27 +19,70 @@ public class EstudianteService {
         this.estudianteRepository = estudianteRepository;
     }
 
-    @Transactional
-    public List<Estudiante> findAll() {
-        return estudianteRepository.findAll();
+    @Transactional(readOnly = true)
+    public List<EstudianteDTO> findAll() {
+        List<Estudiante> estudiantes = estudianteRepository.findAll();
+        return estudiantes.stream().map(
+                e -> EstudianteDTO.builder()
+                        .nombre(e.getNombre())
+                        .apellido(e.getApellido())
+                        .edad(e.getEdad())
+                        .genero(e.getGenero())
+                        .numeroLibreta(e.getNumeroLibreta())
+                        .ciudad(e.getCiudad())
+                        .dni(e.getDni())
+                        .build()).toList();
     }
 
     @Transactional
-    public Estudiante crearEstudiante(EstudianteDTO dto) {
-        return Estudiante.builder()
-                .dni(dto.getDni())
+    public EstudianteDTO crearEstudiante(EstudianteDTO dto) {
+        Estudiante estudianteReq = Estudiante.builder()
                 .nombre(dto.getNombre())
                 .apellido(dto.getApellido())
                 .edad(dto.getEdad())
                 .genero(dto.getGenero())
-                .ciudad(dto.getCiudad())
                 .numeroLibreta(dto.getNumeroLibreta())
+                .ciudad(dto.getCiudad())
+                .dni(dto.getDni())
+                .build();
+        Estudiante estudiante = estudianteRepository.save(estudianteReq);
+        return EstudianteDTO.builder()
+                .nombre(estudiante.getNombre())
+                .apellido(estudiante.getApellido())
+                .edad(estudiante.getEdad())
+                .genero(estudiante.getGenero())
+                .numeroLibreta(estudiante.getNumeroLibreta())
+                .ciudad(estudiante.getCiudad())
+                .dni(estudiante.getDni())
                 .build();
     }
 
-    @Transactional
-    public List<Estudiante> findAllOrdenadosPorApellido() {
-        return estudianteRepository.findAll(Sort.by("apellido"));
+    @Transactional(readOnly = true)
+    public List<EstudianteDTO> findAllOrdenadosPorApellido() {
+        List<Estudiante> estudiantes = estudianteRepository.findAll(Sort.by("apellido"));
+        return estudiantes.stream().map(e -> EstudianteDTO.builder()
+                .nombre(e.getNombre())
+                .apellido(e.getApellido())
+                .edad(e.getEdad())
+                .genero(e.getGenero())
+                .numeroLibreta(e.getNumeroLibreta())
+                .ciudad(e.getCiudad())
+                .dni(e.getDni())
+                .build()).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public EstudianteDTO findByNumeroLibreta(int numeroLibreta) {
+        Estudiante estudiante = estudianteRepository.findByLibretaUni(numeroLibreta);
+        return EstudianteDTO.builder()
+                .nombre(estudiante.getNombre())
+                .apellido(estudiante.getApellido())
+                .edad(estudiante.getEdad())
+                .genero(estudiante.getGenero())
+                .numeroLibreta(estudiante.getNumeroLibreta())
+                .ciudad(estudiante.getCiudad())
+                .dni(estudiante.getDni())
+                .build();
     }
 
     @Transactional(readOnly = true)
@@ -51,7 +94,20 @@ public class EstudianteService {
                 .collect(Collectors.toList());
     }
 
-    private EstudianteDTO convertToDto(Estudiante estudiante){
-        return new EstudianteDTO(estudiante.getNombre(), estudiante.getApellido(),estudiante.getEdad(), estudiante.getGenero(), estudiante.getNumeroLibreta(), estudiante.getCiudad(), estudiante.getDni());
+    private EstudianteDTO convertToDto(Estudiante estudiante) {
+        return new EstudianteDTO(estudiante.getNombre(), estudiante.getApellido(), estudiante.getEdad(), estudiante.getGenero(), estudiante.getNumeroLibreta(), estudiante.getCiudad(), estudiante.getDni());
+    }
+
+    public List<EstudianteDTO> findByGenero(String genero) {
+        List<Estudiante> estudiantes = estudianteRepository.findByGenero(genero);
+        return estudiantes.stream().map(e -> EstudianteDTO.builder()
+                .nombre(e.getNombre())
+                .apellido(e.getApellido())
+                .edad(e.getEdad())
+                .genero(e.getGenero())
+                .numeroLibreta(e.getNumeroLibreta())
+                .ciudad(e.getCiudad())
+                .dni(e.getDni())
+                .build()).toList();
     }
 }
