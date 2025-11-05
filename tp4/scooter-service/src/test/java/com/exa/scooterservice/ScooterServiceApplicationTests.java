76c60b832f7ad105077363bd2b070b2747a9dd96
EmaBrites <ebritesotero@alumnos.exa.unicen.edu.ar>
2025-11-04 22:45:42 -0300

    base tp microservicios

---
+++ tp4/scooter-service/src/test/java/com/exa/scooterservice/ScooterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exa.scooterservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScooterServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
