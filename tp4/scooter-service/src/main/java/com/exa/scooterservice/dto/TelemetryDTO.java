92ad2bc5f5eeb40478a277912e69469481aba7c8
ThomasBandeo <thomas-bandeo@hotmail.com>
2025-11-12 19:29:21 -0300

    Mejoras y correcciones, Endpoints funcionando correctamente y conexión con DB configurada.

--- tp4/scooter-service/src/main/java/com/exa/scooterservice/dto/TelemetryDTO.java
+++ tp4/scooter-service/src/main/java/com/exa/scooterservice/dto/TelemetryDTO.java
@@ -1,12 +1,21 @@
 package com.exa.scooterservice.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Data;
 
 @Data
 public class TelemetryDTO {
+    @NotNull
     private Double latitude;
+
+    @NotNull
     private Double longitude;
+
     // cambios a sumar al acumulado
+    @PositiveOrZero
     private Double kmDelta;        // puede ser 0.0
+
+    @PositiveOrZero
     private Long minutesDelta;     // puede ser 0L
 }
