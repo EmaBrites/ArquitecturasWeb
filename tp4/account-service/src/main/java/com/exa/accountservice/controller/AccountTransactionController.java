74c31a898cd5eff7eca13f396bb64b9bf0c07f54
Matias <matiasschmitz@gmail.com>
2025-11-24 14:13:22 -0300

    mejoras standard a Accounts-ms

--- tp4/account-service/src/main/java/com/exa/accountservice/controller/AccountTransactionController.java
+++ tp4/account-service/src/main/java/com/exa/accountservice/controller/AccountTransactionController.java
@@ -24,7 +24,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 @RestController
-@RequestMapping("/account/transaction")
+@RequestMapping("/accounts/transaction")
 @Tag(name = "Account Transaction Controller", description = "APIs for managing account transactions")
 public class AccountTransactionController {
 
